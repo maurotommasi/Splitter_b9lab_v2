02b215d6ebf3677dac55c74860e9e4a1e64fa770
maurotommasi <mauro.tommasi@live.it>
2021-01-13 14:25:33 +0100

    add getIsRunning, add getOwner

--- contracts/Stoppable.sol
+++ contracts/Stoppable.sol
@@ -8,20 +8,20 @@ contract Stoppable is Owned {
 
     bool private isRunning;
 
-    event RunSwitchLog(address owner, bool switchSetting);
+    event RunSwitchLog(address indexed owner, bool switchSetting);
 
     modifier onlyIfRunning {
         require(isRunning, "Stoppable.onlyIfRunning#001 : It's not running");
         _;
     }
 
+    function getIsRunning() public view returns(bool){
+        return isRunning;
+    }
+
     constructor (bool _isRunning) public {
         isRunning = _isRunning;
     }
-
-    function getIsRunning() public view returns(bool){
-        return isRunning;
-    } 
 
     function runSwitch() public onlyOwner returns(bool){
         bool newRunning = !isRunning;
